f8f88e5cc4dff838d7dc7725040063c6a73875f2
Rifa <rifafairuzzahra@gmail.com>
2025-05-23 14:23:37 +0700

    init server

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import Hapi from '@hapi/hapi'
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
